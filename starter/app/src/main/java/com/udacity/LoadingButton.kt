28141dc2510a920b50012b96fac2c5b0ab158463
Tan Le <tanle@Tans-MacBook-Pro.local>
2020-04-03 23:42:30 -0400

    add animation horizontal progress for button

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -4,14 +4,26 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
+// @JvmOverloads override all constructor of the view class
 class LoadingButton @JvmOverloads constructor(
+
+    // pass in parameter of context, AttributeSet, styleAttr
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 0
-    private var heightSize = 0
+
+    // extends the View class
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var widthSize = 10
+    private var heightSize = 10
 
     private val valueAnimator = ValueAnimator()
 
@@ -19,28 +31,44 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
-
     init {
 
+        // initialize the inflater
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        inflater.inflate(R.layout.custom_button, this, true)
+
+        // get the layout id
+        var textLabel: TextView = findViewById<View>(R.id.labelTextView) as TextView
+
+        // get the reference to styleable
+        val a = context.theme.obtainStyledAttributes(attrs, R.styleable.LoadingButton, 0, 0)
+
+        try {
+            // get and sync the loadingButton text attribute
+
+            var textValue = a.getString(R.styleable.LoadingButton_buttonText)
+            textLabel.setText(textValue)
+
+        }finally{
+
+            //recycle the reference to styleable
+            a.recycle()
+        }
     }
 
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
-        widthSize = w
-        heightSize = h
-        setMeasuredDimension(w, h)
-    }
+    // Override on create custom component/viewgroup
+//    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+//        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+//        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
+//        val h: Int = resolveSizeAndState(
+//            MeasureSpec.getSize(w),
+//            heightMeasureSpec,
+//            0
+//        )
+//        widthSize = w
+//        heightSize = h
+//        setMeasuredDimension(w, h)
+//    }
 
 }
--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.animation.TimeAnimator
 import android.app.DownloadManager
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -7,58 +8,140 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.drawable.ClipDrawable
+import android.graphics.drawable.LayerDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TimeAnimator.TimeListener {
 
     private var downloadID: Long = 0
 
+    // hold an instance to the Notifiation Manager
     private lateinit var notificationManager: NotificationManager
+
+    // hold an instance to the Pending Intent
     private lateinit var pendingIntent: PendingIntent
+
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var mClipDrawable: ClipDrawable
+    private lateinit var mAnimator: TimeAnimator
+    private var mCurrentLevel: Int = 0
+    private lateinit var textLabel: TextView
+
+
+    // to do onCreate lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        // register a BroadcastReceiver to be run in the main thread
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        /**
+         * Look at Button state use with custom button and drawable background
+         */
+        textLabel = findViewById<View>(R.id.labelTextView) as TextView
+        textLabel.text = "Click to Download"
+
+        /**
+         *  Refactor as Animation Setup
+         */
+        val layerDrawable = custom_button.background as LayerDrawable
+        mClipDrawable = layerDrawable.findDrawableByLayerId(R.id.clip_drawable) as ClipDrawable
+
+        mAnimator = TimeAnimator()
+        mAnimator.setTimeListener(this)
+
+
+        // perform download when on custom.button is clicked
         custom_button.setOnClickListener {
-            download()
+            if (!mAnimator.isRunning){
+                textLabel.text = "Downloading"
+                download()
+                mCurrentLevel = 0
+                mAnimator.start()
+            }
+
         }
     }
 
+    override fun onTimeUpdate(animation: TimeAnimator?, totalTime: Long, deltaTime: Long) {
+
+        mClipDrawable.setLevel(mCurrentLevel)
+
+        if (mCurrentLevel >= MAX_LEVEL){
+            mAnimator.cancel()
+            textLabel.text = "Complete"
+        }else{
+            mCurrentLevel = Math.min(MAX_LEVEL, mCurrentLevel + LEVEL_INCREMENT)
+        }
+    }
+
+
+    //    private fun setAnimation() {
+//
+//        var valueAnimator: ValueAnimator = ValueAnimator.ofFloat(0f,-50f)
+//
+//        valueAnimator.addUpdateListener {
+//
+//            val value = it.animatedValue as Float
+//            custom_button.translationY = value
+//
+//        }
+//
+//        valueAnimator.interpolator = LinearInterpolator()
+//        valueAnimator.duration = 10
+//        valueAnimator.start()
+//    }
+
+
     private val receiver = object : BroadcastReceiver() {
+
+        //Upon receiving the Broadcast message assign to the receiver object
         override fun onReceive(context: Context?, intent: Intent?) {
+
+            // create id for the broadcast message of the download message
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
         }
     }
 
     private fun download() {
+
+        // Use DownloadManager to perform a Uri download and send a notification+description upon completing
         val request =
             DownloadManager.Request(Uri.parse(URL))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
+        // get the downloadManger DOWNLOAD_SERVICE
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+        // put the request into the downloadManager's queue
         downloadID =
             downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
+    // Kotlin "static" singleton class object
     companion object {
         private const val URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+        private const val LEVEL_INCREMENT = 400
+        private const val MAX_LEVEL = 10000
     }
 
 }
